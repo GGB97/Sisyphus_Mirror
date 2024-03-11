ee7c1cec5ac66959240bbafd66a0ff2321e4fe98
정원우 <54103715+klik075@users.noreply.github.com>
2024-03-11 19:25:22 +0900

    Rename - 인벤토리

--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 1)]
 public class ItemData : ScriptableObject
 {
     public int width = 1;
