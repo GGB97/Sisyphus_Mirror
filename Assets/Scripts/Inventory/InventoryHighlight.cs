de053054ecf5fe850389eef72ffe81028aa09dca
정원우 <54103715+klik075@users.noreply.github.com>
2024-03-12 16:02:36 +0900

    Feat - 인벤토리간 이동 구현 , 임시 회전 추가

--- Assets/Scripts/Inventory/InventoryHighlight.cs
+++ Assets/Scripts/Inventory/InventoryHighlight.cs
@@ -6,7 +6,11 @@ public class InventoryHighlight : MonoBehaviour
 {
     [SerializeField] RectTransform highlighter;
 
-    public void SetSize(InventoryItem targetItem)
+    public void Show(bool b) //매개변수로 하이라이트를 껐다 킴
+    {
+        highlighter.gameObject.SetActive(b);
+    }
+    public void SetSize(InventoryItem targetItem)//하이라이트의 사이즈를 아이템의 사이즈로 설정
     {
         Vector2 size = new Vector2();
         size.x = targetItem.itemData.width * ItemGrid.TileSizeWidth;
@@ -14,8 +18,24 @@ public class InventoryHighlight : MonoBehaviour
         highlighter.sizeDelta = size;
     }
 
-    public void SetPosition(ItemGrid targetGrid, InventoryItem targetitem)
+    public void SetPosition(ItemGrid targetGrid, InventoryItem targetitem)//하이라이트의 위치를 아이템이 놓일 자리에 설정
     {
+        Vector2 pos = targetGrid.CalculatePositionOnGrid(targetitem, targetitem.onGridPositionX, targetitem.onGridPositionY);
+        highlighter.localPosition = pos;
+    }
+
+    public void SetParent(ItemGrid targetGrid)//그리드 위에 그려질 수 있도록 설정
+    {
+        if (targetGrid == null)
+        {
+            return;
+        }
         highlighter.SetParent(targetGrid.GetComponent<RectTransform>());
+    }
+
+    public void SetPosition(ItemGrid targetGrid, InventoryItem targetitem,int posX,int posY) //
+    {
+        Vector2 pos = targetGrid.CalculatePositionOnGrid(targetitem,posX,posY);
+        highlighter.localPosition = pos;
     }
 }
