1840e6c52283e633b8bbd6d9de474c4a332d4724
정원우 <54103715+klik075@users.noreply.github.com>
2024-03-18 17:59:05 +0900

    Feat : 창고 구현

--- Assets/Scripts/Inventory/ItemDrag.cs
+++ Assets/Scripts/Inventory/ItemDrag.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemDrag : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
 {
     private InventoryController inventoryController;
+    Image image;
     private void Awake()
     {
         inventoryController = InventoryController.Instance;
+        image = GetComponent<Image>();
     }
     public void OnPointerDown(PointerEventData eventData) //마우스를 누른 순간 실행
     {
         if (eventData.button == PointerEventData.InputButton.Left)//마우스 왼쪽 클릭일 때만
         {
             Debug.Log("Click Down");
+            image.raycastTarget = false;
             inventoryController.startPosition = transform.position; ;//시작 위치 지정
             Vector2 mousePostion = Input.mousePosition;
             inventoryController.LeftMouseButtonPress(mousePostion);
@@ -25,6 +29,7 @@ public class ItemDrag : MonoBehaviour,IPointerDownHandler, IPointerUpHandler
     {
         if (eventData.button == PointerEventData.InputButton.Left)//마우스 왼쪽 클릭일 때만
         {
+            image.raycastTarget = true;
             Debug.Log("Click Up");
             inventoryController.LeftMouseButtonPut(transform.position);
         }
