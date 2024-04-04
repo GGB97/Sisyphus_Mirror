4a0fbbeb1005e47c521bb3b9771ff49e7240d207
GGB <rlqja1018@gmail.com>
2024-04-04 15:12:20 +0900

    Feat : 플레이어 강화 DB구현

---
+++ Assets/Scripts/Task/_Base/GameTask.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameTask
+{
+    [SerializeField] protected int _id;
+    [SerializeField] protected string _name;
+    [SerializeField] protected string _desc;
+    [SerializeField] protected TaskType _type;
+    [SerializeField] protected int _target;
+    [SerializeField] protected int _count;
+
+    public int ID => _id;
+    public string Name => _name;
+    public string Desc => _desc;
+    public TaskType Type => _type;
+    public int Target => _target;
+    public int Count => _count;
+}
