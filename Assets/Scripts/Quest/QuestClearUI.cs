ed7518cf582b7d5eda3d7583dae5cd404d93c78a
정원우 <54103715+klik075@users.noreply.github.com>
2024-04-09 17:28:03 +0900

    Feat : 업적 UI Dotween으로 띄우기

--- Assets/Scripts/Quest/QuestClearUI.cs
+++ Assets/Scripts/Quest/QuestClearUI.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class QuestClearUI : MonoBehaviour
 {
     RectTransform canvasRectTransform;
+    Vector2 endPosition;
+    Vector2 startPosition;
+    private void Awake()
+    {
+        startPosition = transform.position;
+        endPosition = new Vector2(transform.position.x, 25f);
+    }
     void Start()
     {
-        
+        StartCoroutine("OpenUi");
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator OpenUi()
     {
-        
+        transform.DOMove(endPosition, 2f);
+
+        yield return new WaitForSeconds(5f);
+
+        yield return StartCoroutine("CloseUi");
+    }
+    public IEnumerator CloseUi() 
+    {
+        transform.DOMove(startPosition, 2f);
+
+        yield return null;
     }
 }
