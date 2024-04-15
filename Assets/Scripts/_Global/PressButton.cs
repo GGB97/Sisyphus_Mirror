9c3d4860bd506e4fedeedbbbb8a347356b0ce466
정원우 <54103715+klik075@users.noreply.github.com>
2024-04-15 12:22:38 +0900

    Feat : 구매 효과음 적용

---
+++ Assets/Scripts/_Global/PressButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PressButton : MonoBehaviour, IPointerDownHandler
+{
+    public string soundTag;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SoundManager.Instance.PlayAudioClip(soundTag);
+    }
+}
