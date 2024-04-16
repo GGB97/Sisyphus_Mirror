07c18557ed70c1a939b7234293ee43dafce4ad2f
GGB <rlqja1018@gmail.com>
2024-04-16 12:33:59 +0900

    Fix : 쉴드 데미지처리 수정

--- Assets/Scripts/Character/Enemies/Enemy.cs
+++ Assets/Scripts/Character/Enemies/Enemy.cs
@@ -333,11 +333,11 @@ public class Enemy : CharacterBehaviour
         DropFielItems(FieldItemType.Gold, dropGoldValue);
 
         float rand = UnityEngine.Random.value;
-        if(rand < EnemyStageModifier.fieldItemDropPer)
+        if (rand < EnemyStageModifier.fieldItemDropPer)
         {
             float rand2 = UnityEngine.Random.value;
 
-            if(rand2 > 0.5f)
+            if (rand2 > 0.5f)
             {
                 DropFielItems(FieldItemType.Heart, 5);
             }
